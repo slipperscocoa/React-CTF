63b372456d9f803b787ba6e494acc359b872a790
slipperscocoa <you@example.com>
2023-07-04 15:10:43 -0400

    fixed routing issues

---
+++ src/Flag1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class FlagOneApp extends React.Component {
+    render() {
+        return (
+            <div>
+              <h1>Testing Flag One</h1>
+            </div>
+        );
+    }
+}
+
+
+export default FlagOneApp;
---
+++ src/Flag2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class FlagTwoApp extends React.Component {
+    render() {
+        return (
+            <div>
+              <h1>Testing Flag TWo</h1>
+            </div>
+        );
+    }
+}
+
+
+export default FlagTwoApp;
